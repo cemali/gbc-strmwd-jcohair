d245020fe3665f2b70dfa1b6db42a13b8e434b4c
Matt Holden <solaris4me@comcast.net>
2012-06-07 14:42:40 -0500

    Initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompleteRoR::Application.config.secret_token = 'ef1ba28c9853311dc65ab764fa56966f7fcba21a55c3f51cdd0fb0f5c83a385c0f48c4bdb32cc35b188f35daafa79f7943037e05032764bf165175b20b753bbb'
