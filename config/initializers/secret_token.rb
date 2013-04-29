3648d346f447bc5a2f5b24af70681b6145fcc52f
Your Name <solaris4me@comcast.net>
2013-04-29 14:56:57 -0500

    adding file structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GbcStrmwdJcohair::Application.config.secret_token = 'af1ffcd1c00a707ccdba112dc2f9f902cd55ea94ecae9177a3c9d488056d49f77c5861b400f3371fb3c062a22ab5ca003bbae2d74925a2f2ac351590ae396071'
