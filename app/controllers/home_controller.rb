c13247d744358ff227b0e4a3f183773010d98961
Matt Holden <holden1473@gmail.com>
2014-05-26 13:16:37 -0500

    my first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    @message="55th Home Going To Heaven Pictorial & Audio Memorial of"
+    @message="55th Home Going To Heaven Pictorial &"
+    @message1="Audio Memorial of"
     @message2="John Cowan O'Hair," 
     @message3="Pastor North Shore Church,"
     @message4="Chicago, IL - 1923 - 1958"
